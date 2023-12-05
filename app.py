63e03779cbcbd6ff07cec49995495e6f567c8d34
felipe966 <felipecastil@gmail.com>
2023-12-05 11:53:35 -0600

    Fix: multithreading file access

--- app.py
+++ app.py
@@ -5,92 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import re
 import sys
-
-
-def inspect_csv(csv_file):
-    # Create a WebDriver instance (no need to specify executable_path)
-    driver = webdriver.Chrome()
-
-    # Load the CSV file
-
-    df = pd.read_csv(csv_file)
-
-    # Ensure the 'Fecha defuncion' column has a string data type
-    df['Fecha defuncion'] = df['Fecha defuncion'].astype(str)
-    df['resultado'] = df['resultado'].astype(str)
-    df['Fecha Nacimiento'] = df['resultado'].astype(str)
-    df['Nombre completo'] = df['resultado'].astype(str)
-    df['Tipo identificacion'] = df['resultado'].astype(str)
-
-    try:
-        for index, row in df.iterrows():
-            id_str=str(row['cedula'])
-            print(index,': ',id_str)
-            tipo_id=inspect_id(id_str)
-            df.at[index, 'Tipo identificacion'] = tipo_id
-            id_str_tipo=id_str+' - '+tipo_id
-
-            if tipo_id not in  ['Persona física','Persona física - Costarricense Naturalización','Persona física - Partida Especial']:
-                continue
-
-            # Navigate to the website
-            driver.get("https://servicioselectorales.tse.go.cr/chc/consulta_cedula.aspx")
-            # Find the input field for the ID number and enter the ID from the CSV
-            id_input = driver.find_element(By.ID, "txtcedula")
-            id_input.clear()  # Clear any existing input
-            id_input.send_keys(id_str)  # Use the 'cedula' column from the CSV
-
-        
-
-            # Click the "Consultar" button
-            consultar_button = driver.find_element(By.ID, "btnConsultaCedula")
-            consultar_button.click()
-
-            time.sleep(2)
-            
-            # Check if the element with ID "lbldefuncion1" exists on the page
-            try:
-                lbl_nombre_completo = driver.find_element(By.ID, "lblnombrecompleto")
-                df.at[index, 'Nombre completo'] = lbl_nombre_completo.text
-                id_str_tipo+=' - '+lbl_nombre_completo.text
-                lbl_fecha_nacimiento = driver.find_element(By.ID, "lblfechaNacimiento")
-                df.at[index, 'Fecha Nacimiento'] = lbl_fecha_nacimiento.text
-                id_str_tipo+=' - '+lbl_fecha_nacimiento.text
-                lbl_defuncion_element = driver.find_element(By.ID, "lbldefuncion1")
-                lbl_defuncion2_element = driver.find_element(By.ID, "lbldefuncion2")
-                if lbl_defuncion_element.text.strip():  # Check if the element has non-empty text
-                    df.at[index, 'resultado'] = "FALLECIDO"
-                    df.at[index, 'Fecha defuncion'] = lbl_defuncion2_element.text
-                    print(f"{id_str_tipo} - Fallecido en "+lbl_defuncion2_element.text)
-                
-            except NoSuchElementException:
-                try:
-                    lblMensaje2 = driver.find_element(By.ID, "lblmensaje1")
-                    if lblMensaje2.text ==  "No se encontró información en la base de datos registral civil que coincida con los datos aportados.":
-                        df.at[index, 'resultado'] = "No se encontró información en la base de datos"
-                        print(f"{id_str_tipo} - No se encontró información en la base de datos")
-                except NoSuchElementException:
-                    try:
-                        lblmensajes = driver.find_element(By.ID, "lblmensajes")
-                        if lblmensajes.text ==  "Por favor ingrese una cédula válida":
-                            df.at[index, 'resultado'] = "Cédula invalida"
-                            print(f"{id_str_tipo} - Cédula invalida")
-                    except NoSuchElementException:
-                        df.at[index, 'resultado'] = "Sin fecha defuncion"
-                        print(f"{id_str_tipo} - Sin fecha de defuncion")
-
-            # Wait for n seconds before proceeding to the next query
-            time.sleep(1)
-
-    except Exception as e:
-        print("An error occurred:", str(e))
-
-    finally:
-        # Close the browser window
-        driver.quit()
-
-    # Save the updated DataFrame back to the CSV file
-    df.to_csv(csv_file, index=False)
+import concurrent.futures
 
 def inspect_id(identificacion):
     # Expresiones regulares para verificar los formatos
@@ -117,12 +32,148 @@ def inspect_id(identificacion):
     else:
         return "Tipo de identificación desconocido"
 
+def inspect_csv_partial(df):
+    driver = webdriver.Chrome()
+    
+    # Load the CSV file
+
+
+    # Ensure the 'Fecha defuncion' column has a string data type
+    df['Fecha defuncion'] = df['Fecha defuncion'].astype(str)
+    df['resultado'] = df['resultado'].astype(str)
+    df['Fecha Nacimiento'] = df['resultado'].astype(str)
+    df['Nombre completo'] = df['resultado'].astype(str)
+    df['Tipo identificacion'] = df['resultado'].astype(str)
+
+    try:
+        for index, row in df.iterrows():
+            # id_str=''
+            # id_str_tipo=''
+            # df.at[index, 'resultado'] = 'nan'
+            # df.at[index, 'Fecha defuncion'] = 'nan'
+            # df.at[index, 'Tipo identificacion'] = 'nan'
+            # df.at[index, 'Nombre completo'] = 'nan'
+            # df.at[index, 'Fecha Nacimiento'] = 'nan'
+
+            id_str = str(row['cedula'])
+            #print(index, ': ', id_str)
+            tipo_id = inspect_id(id_str)
+            df.at[index, 'Tipo identificacion'] = tipo_id
+            id_str_tipo = str(index)+': '+id_str + ' - ' + tipo_id
+
+            if tipo_id not in ['Persona física', 'Persona física - Costarricense Naturalización', 'Persona física - Partida Especial']:
+                continue
+
+            driver.get("https://servicioselectorales.tse.go.cr/chc/consulta_cedula.aspx")
+            id_input = driver.find_element(By.ID, "txtcedula")
+            id_input.clear()
+            id_input.send_keys(id_str)
+
+            consultar_button = driver.find_element(By.ID, "btnConsultaCedula")
+            consultar_button.click()
+
+            time.sleep(2)
+
+            try:
+                lbl_nombre_completo = driver.find_element(By.ID, "lblnombrecompleto")
+                df.at[index, 'Nombre completo'] = lbl_nombre_completo.text
+                id_str_tipo += ' - ' + lbl_nombre_completo.text
+                lbl_fecha_nacimiento = driver.find_element(By.ID, "lblfechaNacimiento")
+                df.at[index, 'Fecha Nacimiento'] = lbl_fecha_nacimiento.text
+                id_str_tipo += ' - ' + lbl_fecha_nacimiento.text
+                lbl_defuncion_element = driver.find_element(By.ID, "lbldefuncion1")
+                lbl_defuncion2_element = driver.find_element(By.ID, "lbldefuncion2")
+                if lbl_defuncion_element.text.strip():
+                    df.at[index, 'resultado'] = "FALLECIDO"
+                    df.at[index, 'Fecha defuncion'] = lbl_defuncion2_element.text
+                    print(f"{id_str_tipo} - Fallecido en " + lbl_defuncion2_element.text)
+
+            except NoSuchElementException:
+                try:
+                    lblMensaje2 = driver.find_element(By.ID, "lblmensaje1")
+                    if lblMensaje2.text == "No se encontró información en la base de datos registral civil que coincida con los datos aportados.":
+                        df.at[index, 'resultado'] = "No se encontró información en la base de datos"
+                        print(f"{id_str_tipo} - No se encontró información en la base de datos")
+                except NoSuchElementException:
+                    try:
+                        lblmensajes = driver.find_element(By.ID, "lblmensajes")
+                        if lblmensajes.text == "Por favor ingrese una cédula válida":
+                            df.at[index, 'resultado'] = "Cédula invalida"
+                            print(f"{id_str_tipo} - Cédula invalida")
+                    except NoSuchElementException:
+                        df.at[index, 'resultado'] = "Sin fecha defuncion"
+                        print(f"{id_str_tipo} - Sin fecha de defuncion")
+
+            time.sleep(1)
+    except Exception as e:
+        print("An error occurred:", str(e))
+    finally:
+        driver.quit()
+
+    return df
+
+def inspect_csv_multi_threaded(csv_file, num_threads):
+    df = pd.read_csv(csv_file)
+    total_rows = len(df)
+
+    # Divide the DataFrame into parts to be processed by each thread
+    chunk_size = total_rows // num_threads
+    futures = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        for i in range(num_threads):
+            start = i * chunk_size
+            end = start + chunk_size if i < num_threads - 1 else total_rows
+            df_part = df.iloc[start:end].copy()  # Create a copy of the DataFrame part
+            futures.append(executor.submit(inspect_csv_partial, df_part))
+
+    # Wait for all threads to finish
+    for future in concurrent.futures.as_completed(futures):
+        if future.exception() is not None:
+            print("Error in thread:", future.exception())
+
+    # Concatenate the results from all threads
+    result_df = pd.concat([future.result() for future in futures], ignore_index=True)
+
+    # Write the result DataFrame to a CSV file
+    result_df.to_csv('resultado_' + csv_file, index=False)
+
+def inspect_csv_multi_subprocess(csv_file, num_processes):
+    df = pd.read_csv(csv_file)
+    total_rows = len(df)
+
+    # Divide el DataFrame en partes iguales para cada subprocess
+    chunk_size = total_rows // num_processes
+    futures = []
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=num_processes) as executor:
+        for i in range(num_processes):
+            start = i * chunk_size
+            end = start + chunk_size if i < num_processes - 1 else total_rows
+            futures.append(executor.submit(inspect_csv_partial, csv_file, start, end))
+
+    # Espera a que todos los subprocesses finalicen
+    for future in concurrent.futures.as_completed(futures):
+        if future.exception() is not None:
+            print("Error in subprocess:", future.exception())
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python script.py nombre_archivo.csv")
+    if len(sys.argv) != 3:
+        print("Uso: python script.py nombre_archivo.csv num_threads")
     else:
         archivo_csv = sys.argv[1]
-        inspect_csv(archivo_csv)
+        num_threads = int(sys.argv[2])
 
+        # Record the start time
+        start_time = time.time()
+
+        inspect_csv_multi_threaded(archivo_csv, num_threads)
+
+        # Record the end time
+        end_time = time.time()
+
+        # Calculate and print the total time taken
+        total_time = end_time - start_time
+        print(f"Total time taken: {total_time} seconds")
 
